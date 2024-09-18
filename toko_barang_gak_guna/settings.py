72f967625321f5693392326034661d74767b9cb9
Juan <juanlukius1512@gmail.com>
2024-09-18 14:52:20 +0700

    mencoba menambahkan url allowedhost

--- toko_barang_gak_guna/settings.py
+++ toko_barang_gak_guna/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l8hb8o0f-^pz6bnle3&&$wj&6tg5*@0ei+y0pkgj%herup+&y#
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost", "127.0.0.1","http://pbp.cs.ui.ac.id/juan.lukius/tokobaranggakguna","https://pbp.cs.ui.ac.id/juan.lukius/tokobaranggakguna", "http://juan-lukius-tokobaranggakguna.pbp.cs.ui.ac.id/"
+    "localhost", "127.0.0.1","http://pbp.cs.ui.ac.id/juan.lukius/tokobaranggakguna","https://pbp.cs.ui.ac.id/juan.lukius/tokobaranggakguna", "http://juan-lukius-tokobaranggakguna.pbp.cs.ui.ac.id/","juan-lukius-tokobaranggakguna.pbp.cs.ui.ac.id"
 ]
 
 
